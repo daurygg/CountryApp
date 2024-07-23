11e185b3360d8371badbeeebeb212e379395b8a9
devgg03 <103075382+daurygg@users.noreply.github.com>
2024-07-23 17:58:36 -0400

    changes

--- src/app/countries/Service/countries.service.ts
+++ src/app/countries/Service/countries.service.ts
@@ -20,8 +20,19 @@ export class CountriesService {
     byRegion:    {region:'', Countries:[]}
     };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadFromLocalStorage();
+  }
 
+
+  private saveToLocalStorage():void{
+    localStorage.setItem('cacheStore', JSON.stringify(this.cacheStore));
+  }
+
+  private loadFromLocalStorage():void{
+    if(!localStorage.getItem('cacheStore')) return;
+    this.cacheStore = JSON.parse(localStorage.getItem('cacheStore')!);
+  };
   searchCountryByAlphaCode( code: string ): Observable<Country | null> {
     const url = `${ this.Url }/alpha/${ code }`;
     return this.http.get<Country[]>( url )
@@ -38,19 +49,22 @@ export class CountriesService {
   searchCapital(term: string): Observable<Country[]>{
     const url = `${this.Url}/capital/${term}`;
     return this.SetCountryurl(url).pipe(
-      tap( Countries => this.cacheStore.byCapital = {term, Countries})
+      tap( Countries => this.cacheStore.byCapital = {term, Countries}),
+      tap(()=> this.saveToLocalStorage())
     );
   }
   SearchCountry(term: string): Observable<Country[]>{
     const url = `${this.Url}/name/${term}`
     return this.SetCountryurl(url).pipe(
-      tap( Countries => this.cacheStore.byCountries = {term, Countries})
+      tap( Countries => this.cacheStore.byCountries = {term, Countries}),
+      tap(()=> this.saveToLocalStorage())
     );
   }
   SearchRegion(region: Region){
     const url = `${this.Url}/region/${region}`
     return this.SetCountryurl(url).pipe(
-      tap( Countries => this.cacheStore.byRegion = {region, Countries})
+      tap( Countries => this.cacheStore.byRegion = {region, Countries}),
+      tap(()=> this.saveToLocalStorage())
     );;
   }
 }
